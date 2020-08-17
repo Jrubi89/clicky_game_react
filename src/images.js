d6f017dbaa221560c9fe4a927a0363b3abda6f74
Jrubi89 <62040443+Jrubi89@users.noreply.github.com>
2020-08-17 17:02:22 -0400

    Add files via upload

---
+++ src/images.js
@@ -0,0 +1,30 @@
+import A1 from "./images/A1.jpg";
+import A2 from "./images/A2.jpg";
+import A3 from "./images/A3.jpg";
+import A4 from "./images/A4.jpg";
+import A5 from "./images/A5.jpg";
+import A6 from "./images/A6.jpg";
+import A7 from "./images/A7.jpg";
+import A8 from "./images/A8.jpg";
+import A9 from "./images/A9.jpg";
+import A10 from "./images/A10.jpg";
+import A11 from "./images/A11.jpg";
+import A12 from "./images/A12.jpg";
+
+
+const images = [
+   A1,
+   A2,
+   A3,
+   A4,
+   A5,
+   A6,
+   A7,
+   A8,
+   A9,
+   A10,
+   A11,
+   A12
+  ];
+  
+  export default images;
